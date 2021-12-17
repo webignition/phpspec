4515dde879af6c532170bd311d35c5654ca23e55
Jon Cram <webignition@gmail.com>
2021-12-17 11:27:32 +0000

    Import PHP 8.1 fixes from jaylinski/phpspec/commit/a51f6bf1e88014820b3141249af46e4827256eda

--- src/PhpSpec/Console/Command/RunCommand.php
+++ src/PhpSpec/Console/Command/RunCommand.php
@@ -142,7 +142,7 @@ EOF
         ;
     }
 
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $container = $this->getApplication()->getContainer();
@@ -172,7 +172,7 @@ EOF
 
         $container->configure();
 
-        $locator = $input->getArgument('spec');
+        $locator = $input->getArgument('spec') ?? '';
         $linenum = null;
         if (preg_match('/^(.*)\:(\d+)$/', $locator, $matches)) {
             list($_, $locator, $linenum) = $matches;
